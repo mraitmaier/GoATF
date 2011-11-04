fd9b273eaa9aacd27ee9ed3a2daca8fecca15a42
miran <miran@mraitmaier1.eion.si>
2011-11-04 11:30:03 +0100

    Function CopyFile() added.

--- utils/file.go
+++ utils/file.go
@@ -12,6 +12,7 @@ import (
 	"io/ioutil"
 	"strings"
 	"bufio"
+    "io"
 )
 
 /*
@@ -67,4 +68,20 @@ func ReadLines(filename string) (lines []string, err os.Error) {
 	// now we convert the text into an array of lines
 	lines = strings.Split(string(data), "\n")
 	return
+}
+
+func CopyFile(dst, src string) (int64, os.Error) {
+    sf, err := os.Open(src)
+    if err != nil {
+        return 0, err
+    }
+    defer sf.Close()
+
+    df, err := os.Create(dst)
+    if err != nil {
+        return 0, err
+    }
+    defer df.Close()
+
+    return io.Copy(df, sf)
 }
